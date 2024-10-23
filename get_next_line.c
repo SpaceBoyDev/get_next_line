b29a97082bce3a9c36bdccfce44a2873f7be1e19
elguay <dario.mart.lop.03@gmail.com>
2024-10-23 20:01:57 +0200

    edit: little changes :3

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: dario <dario@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 19:25:23 by darmarti          #+#    #+#             */
-/*   Updated: 2024/10/23 19:53:08 by dario            ###   ########.fr       */
+/*   Updated: 2024/10/23 20:01:37 by dario            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_cleanline(char *line)
+static char	*ft_cleanline(char *line)
 {
 	char	*clean_line;
 	size_t	i;
@@ -20,7 +20,6 @@ char	*ft_cleanline(char *line)
 	i = 0;
 	while (line[i] != '\n' && line[i])
 		++i;
-
 	clean_line = (char *)ft_calloc(i + 2, sizeof(char));
 	if (!clean_line)
 		return (NULL);
@@ -34,27 +33,7 @@ char	*ft_cleanline(char *line)
 	return (clean_line);
 }
 
-// char	*ft_cleanbuffer(char *buffer)
-// {
-// 	char	*clean_buffer;
-// 	size_t	i;
-
-// 	i = 0;
-// 	while (buffer[i] != '\n')
-// 		++i;
-// 	clean_buffer = ft_calloc(i + 2, sizeof(char));
-// 	if (!clean_buffer)
-// 		return (NULL);
-// 	i = 0;
-// 	while (buffer[i - 1] != '\n')
-// 	{
-// 		clean_buffer[i] = buffer[i];
-// 		++i;
-// 	}
-// 	return (clean_buffer);
-// }
-
-char	*ft_clean_last_buffer(char *buffer)
+static char	*ft_clean_last_buffer(char *buffer)
 {
 	char	*new_last_buffer;
 	size_t	len;
@@ -74,7 +53,6 @@ char	*ft_clean_last_buffer(char *buffer)
 	j = 0;
 	while (buffer[i] != '\0')
 		new_last_buffer[j++] = buffer[i++];
-	//printf("\n\nnew_last_buffer -> %s\n\n", new_last_buffer);
 	return (new_last_buffer);
 }
 
@@ -90,11 +68,7 @@ char	*get_next_line(int fd)
 	buffer = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	if (fd < 0 || fd > 1024 || BUFFER_SIZE <= 0 || !buffer || !last_buffer[fd])
 		return (NULL);
-
-	//printf("\nfd -> %d\n\n", fd);
-	//line = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	line = ft_strjoin(NULL, last_buffer[fd]);
-
 	bytes_read = 1;
 	while (!ft_strchr(buffer, '\n') && bytes_read > 0 && !ft_strchr(line, '\n'))
 	{
@@ -106,14 +80,8 @@ char	*get_next_line(int fd)
 		line = ft_strjoin(line, buffer);
 		last_buffer[fd] = ft_strjoin(last_buffer[fd], buffer);
 	}
-
 	line = ft_cleanline(line);
-
-	//line = ft_strjoin(line, ft_cleanbuffer(buffer));
-	//last_buffer[fd] = (NULL, buffer);
-
 	last_buffer[fd] = ft_clean_last_buffer(last_buffer[fd]);
 	free(buffer);
-
 	return (line);
 }
